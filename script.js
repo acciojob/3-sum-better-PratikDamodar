4b6efe71d4f87109487f2de56ccbee320396adeb
PratikDamodar <117561545+PratikDamodar@users.noreply.github.com>
2023-04-30 11:02:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function threeSum(arr, target) {
 // write your code here
-	arr.sort((ab)=>a-b);
+	arr.sort((a,b)=>a-b);
 	let closetSum = Infinity;
 	for(let i=0;i<arr.length-2;i++){
 		let j =i+1;
+		let k= arr.length-1;
 		while(j<k){
 			const sum =arr[i]+arr[j]+arr[k];
 			if(Math.abs(sum-target)< Math.abs(closetSum-target)){
